2ec91e8b7ca8d73286ecb1356558fbfd7a1a223e
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-07-25 01:24:58 +0800

    🔧 gh-nvim configuration

---
+++ lua/plugins/litee.lua
@@ -0,0 +1,31 @@
+require('litee.lib').setup({
+    tree = {
+        icon_set = "codicons"
+    },
+    panel = {
+        orientation = "right",
+        panel_size  = 30
+    },
+    term = {
+        position = "bottom",
+        term_size = 15,
+    }
+})
+require('litee.filetree').setup({
+    hide_cursor = false,
+})
+require('litee.symboltree').setup({
+    icon_set = "codicons",
+    hide_cursor = false,
+})
+require('litee.calltree').setup({
+    icon_set = "codicons",
+    hide_cursor = false,
+})
+require('litee.bookmarks').setup({
+})
+
+require('litee.gh').setup({
+    icon_set = "codicons",
+    map_resize_keys = true
+})
