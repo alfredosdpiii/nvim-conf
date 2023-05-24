428b16b2d891342084a61ff92fce92adbd68c34c
alfredosdpiii <alfredo.sdpiii@gmail.com>
2023-05-24 23:45:14 +0800

    added new colorscheme

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -11,6 +11,16 @@ return {
     -- end,
   },
   {
+    "hachy/eva01.vim",
+    lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme "eva01"
+    -- or
+    -- vim.cmd.colorscheme "eva01-LCL"
+    -- end,
+  },
+  {
     'nyngwang/nvimgelion',
     lazy = false,
     priority = 1000,
