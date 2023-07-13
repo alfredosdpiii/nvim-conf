61bc948bed8a5b05856afdcf1a316621fd56a859
alfredosdpiii <alfredo.sdpiii@gmail.com>
2023-07-14 01:19:41 +0800

    added custom diags but not working

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -11,8 +11,43 @@ local handlers = {
   ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = EcoVim.ui.float.border }),
   ["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics,
-    { virtual_text = EcoVim.lsp.virtual_text }
+    { virtual_text = true }
   ),
+  -- ["textDocument/publishDiagnostics"] = function(
+  --   _,
+  --   result,
+  --   ctx,
+  --   config
+  -- )
+  --   if result.diagnostics == nil then
+  --     return
+  --   end
+  --
+  --   -- ignore some tsserver diagnostics
+  --   local idx = 1
+  --   while idx <= #result.diagnostics do
+  --     local entry = result.diagnostics[idx]
+  --
+  --     local formatter = require('format-ts-errors')[entry.code]
+  --     entry.message = formatter and formatter(entry.message) or entry.message
+  --
+  --     -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+  --     if entry.code == 80001 then
+  --       -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+  --       table.remove(result.diagnostics, idx)
+  --     else
+  --       idx = idx + 1
+  --     end
+  --   end
+  --
+  --   vim.lsp.diagnostic.on_publish_diagnostics(
+  --     _,
+  --     result,
+  --     ctx,
+  --     config
+  --   )
+  --   { virtual_text = true }
+  -- end,
   ["textDocument/definition"] = function(err, result, method, ...)
     P(result)
     if vim.tbl_islist(result) and #result > 1 then
